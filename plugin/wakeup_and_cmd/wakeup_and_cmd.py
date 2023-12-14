56f41e3bf7b8dac4c0fcded29bf0d48ce5813802
Mikhail Menshchikov <mikhail.menshchikov@akvelon.com>
2023-12-14 13:57:40 +0100

    error handling

--- plugin/wakeup_and_cmd/wakeup_and_cmd.py
+++ plugin/wakeup_and_cmd/wakeup_and_cmd.py
@@ -1,18 +1,23 @@
 import os
-from talon import Module, actions, speech_system, cron
+import traceback
+from talon import actions, app, speech_system, cron
+
+RECORDINGS_FOLDER = "recordings"
 
 talon_home = actions.path.talon_home()
 
-mod = Module()
-
 
 def get_last_voice_files(max: int):
-    recordings_path = os.path.join(talon_home, "recordings")
-    all_files = os.listdir(recordings_path)
-    all_files.sort(key=lambda x: os.path.getctime(
-        os.path.join(recordings_path, x)), reverse=True)
+    try:
+        recordings_path = os.path.join(talon_home, RECORDINGS_FOLDER)
+        all_files = os.listdir(recordings_path)
+        all_files.sort(key=lambda x: os.path.getctime(
+            os.path.join(recordings_path, x)), reverse=True)
 
-    return [os.path.join(recordings_path, file) for file in all_files[:max]]
+        return [os.path.join(recordings_path, file) for file in all_files[:max]]
+    except FileNotFoundError:
+        traceback.print_exc()
+        return []
 
 
 def get_last_voice_file():
@@ -24,14 +29,20 @@ def get_last_voice_file():
 def repeat_wake():
     path = get_last_voice_file()
     actions.speech.enable()
-    actions.sleep("200ms")
-    actions.speech.replay(path)
+
+    if path:
+        actions.sleep("200ms")
+        actions.speech.replay(path)
+    else:
+        print("Voice file not found")
+        app.notify("Voice file not found")
 
 
 def pre_phrase(e):
-    cmd = e["text"]
     if actions.speech.enabled():
         return
+
+    cmd = e["text"]
 
     if cmd == ["wake", "up"]:
         cron.after("200ms", actions.speech.enable)
